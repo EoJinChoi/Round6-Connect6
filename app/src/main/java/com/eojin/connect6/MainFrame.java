89254edfcc88c2d5dd5aac58120e2d76baac3bd4
01031 <01031@172.18.142.238>
2022-07-04 13:41:39 +0900

    Do the initial commit

---
+++ app/src/main/java/com/eojin/connect6/MainFrame.java
@@ -0,0 +1,12 @@
+package com.eojin.connect6;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
